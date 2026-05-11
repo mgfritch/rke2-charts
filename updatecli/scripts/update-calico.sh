cc530b6e7f14df8ba8b72f6fc860f35a7f2c4341
Michael Fritch <mfritch@suse.com>
2026-05-11 14:45:05 -0600

    fix broken script

--- updatecli/scripts/update-calico.sh
+++ updatecli/scripts/update-calico.sh
@@ -7,27 +7,33 @@ ISSUE_TITLE="Updatecli failed for calico ${CALICO_VERSION}"
 trap report-error EXIT INT
 
 if [ -n "$CALICO_VERSION" ]; then
-	current_calico_version=$(yq '.version' packages/rke2-calico/templates/crd-template/Chart.yaml)
+	GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-calico' make prepare
+	GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-calico-crd' make prepare
+	current_calico_version=$(yq '.version' packages/rke2-calico-crd/charts/Chart.yaml)
 	if [ "$current_calico_version" != "$CALICO_VERSION" ]; then
 		echo "Updating Calico chart to $CALICO_VERSION"
+
 		mkdir workdir
 		wget -P workdir/ https://github.com/projectcalico/calico/releases/download/$CALICO_VERSION/tigera-operator-$CALICO_VERSION.tgz
 		tar --directory=workdir -xf workdir/tigera-operator-$CALICO_VERSION.tgz tigera-operator/values.yaml
 		current_tigera_operator_version=$(yq '.tigeraOperator.version' workdir/tigera-operator/values.yaml)
 		rm -fr workdir
-		sed -i "s/ version: .*/ version: $CALICO_VERSION/g" packages/rke2-calico/generated-changes/patch/Chart.yaml.patch
-		sed -i "s/   version: .*/   version: $current_tigera_operator_version/g" packages/rke2-calico/generated-changes/patch/values.yaml.patch
-		sed -i "s/   tag: .*/   tag: $CALICO_VERSION/g" packages/rke2-calico/generated-changes/patch/values.yaml.patch
-		sed -i "s/ appVersion: .*/ appVersion: $CALICO_VERSION/g" packages/rke2-calico-crd/generated-changes/patch/Chart.yaml.patch
-		sed -i "s/ version: .*/ version: $CALICO_VERSION/g" packages/rke2-calico-crd/generated-changes/patch/Chart.yaml.patch
+
+		yq -i ".version = \"$CALICO_VERSION\"" packages/rke2-calico/charts/Chart.yaml
+		yq -i ".appVersion = \"$CALICO_VERSION\"" packages/rke2-calico/charts/Chart.yaml
+		yq -i ".tigeraOperator.version = \"$current_tigera_operator_version\"" packages/rke2-calico/charts/values.yaml
+		yq -i ".calicoctl.tag = \"$CALICO_VERSION\"" packages/rke2-calico/charts/values.yaml
 		yq -i ".url = \"https://github.com/projectcalico/calico/releases/download/$CALICO_VERSION/tigera-operator-$CALICO_VERSION.tgz\" |
 			.packageVersion = 00" packages/rke2-calico/package.yaml
+
+		yq -i ".version = \"$CALICO_VERSION\"" packages/rke2-calico-crd/charts/Chart.yaml
+		yq -i ".appVersion = \"$CALICO_VERSION\"" packages/rke2-calico-crd/charts/Chart.yaml
 		yq -i ".url = \"https://github.com/projectcalico/calico/releases/download/$CALICO_VERSION/crd.projectcalico.org.v1-$CALICO_VERSION.tgz\" |
 			.packageVersion = 00" packages/rke2-calico-crd/package.yaml
-		GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-calico' make prepare
-		GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-calico-crd' make prepare
+
 		find packages/rke2-calico/charts -name '*.orig' -delete
 		find packages/rke2-calico-crd/charts -name '*.orig' -delete
+
 		GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-calico' make patch
 		GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-calico-crd' make patch
 		make clean
